5ec4e47dffbf6583323e282aa590a7541d59662b
Michael Rockingham <mrockingham2@gmail.com>
2020-09-18 13:32:05 -0400

    mvp

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,40 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        
+        # start with light on
+        self.set_light_on()
+
+        while self.light_is_on():
+
+
+
+                #turn light off so robot can start
+            self.set_light_off()
+
+            while self.can_move_right():
+
+                self.move_right()
+
+                if self.compare_item() == 1:
+
+                    self.swap_item()
+                    self.set_light_on()
+
+            while self.can_move_left():
+
+                self.swap_item()
+
+                self.move_left()
+
+
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+
+                self.move_right()
+                self.swap_item()
+                self.move_left()
 
 
 if __name__ == "__main__":
